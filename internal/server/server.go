b27215b93547466840cde852b08023c843e1fcc5
Maksym Hryhoriev <zercos2106@gmail.com>
2024-12-27 22:10:06 +0100

    Add server package

---
+++ internal/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+)
+
+func CreateServer() *echo.Echo {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Logger.SetLevel(log.DEBUG)
+
+	// Routes
+	e.GET("/", index)
+	return e
+}
+
+func Run(e *echo.Echo) {
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func index(c echo.Context) error {
+	response := map[string]string{
+		"message": "Welcome to the OAuth-Tower - OAuth 2.0 Authorization Server",
+		"status":  "running",
+		"config":  "https://example.com/.well-known/oauth-authorization-server",
+	}
+	return c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -1,30 +1,10 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"github.com/labstack/gommon/log"
+	"github.com/zercos/oauth-tower/internal/server"
 )
 
 func main() {
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Logger.SetLevel(log.DEBUG)
-
-	// Routes
-	e.GET("/", index)
-
-	e.Logger.Fatal(e.Start(":8000"))
-}
-
-func index(c echo.Context) error {
-	response := map[string]string{
-		"message": "Welcome to the OAuth-Tower - OAuth 2.0 Authorization Server",
-		"status":  "running",
-		"config":  "https://example.com/.well-known/oauth-authorization-server",
-	}
-	return c.JSON(http.StatusOK, response)
+	serv := server.CreateServer()
+	server.Run(serv)
 }
